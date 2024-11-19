08ec877bb81306232ea7f689820e1a5bd21a12ad
kyddaniel <mksuenaa@connect.ust.hk>
2024-11-19 23:01:27 +0800

    Implement "Primary" and "Qualifier"

--- src/main/java/org/springDemo/config/AppConfig.java
+++ src/main/java/org/springDemo/config/AppConfig.java
@@ -3,14 +3,18 @@ package org.springDemo.config;
 import org.springDemo.Alien;
 import org.springDemo.Computer;
 import org.springDemo.Desktop;
+import org.springDemo.Laptop;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = {"desktop1", "desktop2"})
+    @Primary // will be selected at first if multiple implementation of an interface exist
     //@Bean <= default name is method name (i.e. desktop)
     //@Scope("prototype")
     public Desktop desktop() {
@@ -18,11 +22,16 @@ public class AppConfig {
     }
 
     @Bean
-    public Alien alien(Computer com) {
+    public Alien alien(@Qualifier("desktop1") Computer com) {
         Alien obj = new Alien();
         obj.setAge(21);
         obj.setCom(com);
         return obj;
-    };
+    }
+
+    @Bean
+    public Laptop laptop() {
+        return new Laptop();
+    }
 
 }
