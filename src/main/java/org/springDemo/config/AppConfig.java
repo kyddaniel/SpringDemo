e03d3b63c5d21e23cb39258ce650addbdd0a2d21
kyddaniel <mksuenaa@connect.ust.hk>
2024-11-19 22:36:06 +0800

    Implement "Scope" annotation

--- src/main/java/org/springDemo/config/AppConfig.java
+++ src/main/java/org/springDemo/config/AppConfig.java
@@ -3,12 +3,14 @@ package org.springDemo.config;
 import org.springDemo.Desktop;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = {"desktop1", "desktop2"})
     //@Bean <= default name is method name (i.e. desktop)
+    @Scope("prototype")
     public Desktop desktop() {
         return new Desktop();
     };
