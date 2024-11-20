193a9b0fb924af3de781cbf7876b1e992b13a9a9
kyddaniel <mksuenaa@connect.ust.hk>
2024-11-20 22:30:30 +0800

    Implement "Scope" and "Value" annotation

--- src/main/java/org/springDemo/Alien.java
+++ src/main/java/org/springDemo/Alien.java
@@ -2,6 +2,7 @@ package org.springDemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.beans.ConstructorProperties;
@@ -9,6 +10,7 @@ import java.beans.ConstructorProperties;
 @Component
 public class Alien {
 
+    @Value("21") // inject value to the field
     private int age;
 
     @Autowired // field injection
--- src/main/java/org/springDemo/Laptop.java
+++ src/main/java/org/springDemo/Laptop.java
@@ -1,10 +1,12 @@
 package org.springDemo;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Scope("prototype") // change the scope of the bean
 public class Laptop implements Computer {
 
     public Laptop() {
